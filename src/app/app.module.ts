879f9a4dc00fb8fc5b4969f795ec0a7c841004ac
Darkoson <darkothomas80@gmail.com>
2021-09-19 23:54:44 +0000

    creation of two component: counter-button and counter-output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/component/counter/counter.component';
+import { CounterOutputComponent } from './counter/component/counter-output/counter-output.component';
+import { CounterButtonComponent } from './counter/component/counter-button/counter-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    CounterOutputComponent,
+    CounterButtonComponent
   ],
   imports: [
     BrowserModule
