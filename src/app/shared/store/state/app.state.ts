d0406d1b1a3c5528927578a860c7e3dbf32b5ca3
Darkoson <darkothomas80@gmail.com>
2021-09-21 00:14:25 +0000

    refactoring of folder structure in the post module

--- src/app/shared/store/state/app.state.ts
+++ src/app/shared/store/state/app.state.ts
@@ -1,7 +1,7 @@
 import { counterReducer } from "src/app/counter/store/reducer/counter.reducer";
 import { CounterStateInterface } from "src/app/counter/store/state/counter.state";
-import { postReducer } from "src/app/post/component/post-list/store/reducer/post.reducer";
-import { PostStateInterface } from "src/app/post/component/post-list/store/state/post.state";
+import { postReducer } from "src/app/post/store/reducer/post.reducer";
+import { PostStateInterface } from "src/app/post/store/state/post.state";
 
 export interface AppState{
     counter: CounterStateInterface
